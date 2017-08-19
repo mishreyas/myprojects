864f7588bf471b46402477af3e23b1d10fcd53be
mishreyas <31148183+mishreyas@users.noreply.github.com>
2017-08-18 19:28:43 -0700

    JavaScript
     My first commit to master.

---
+++ JavaBrains/scratchpad.js
@@ -0,0 +1,28 @@
+var person = {
+  "firstName": "Shreyas",
+  "lastName" : "Bhosale",
+  "getFullName" : function(){
+  return this.firstName + " " + this.lastName;
+  },
+  "address":{
+    "street": "2452 Nutwood Ave",
+    "city": "Fullerton",
+    "state": "California",
+    
+  },
+  "isFromState": function(sts){
+      return(this.address.state === sts)
+    }
+
+};
+
+//var fullName = person.getFullName();
+//console.log(fullName);
+//var person2 = person;
+
+//person = {};
+
+//console.log(person2.getFullName());
+
+console.log(person.isFromState("California"));
+
